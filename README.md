8ef55e9f799c5aef6717d12e79b3ce4bc9145055
Joerg Stephan <jstephan@playbk>
2015-03-18 16:01:32 +0100

    v1.0.0.a

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# SendMeSpamIDS.py
+README - SendMeSpamIDS.py
+====
 Simple SMTP fetch all IDS and analyzer
+
+## Modules needed
+'''
+import smtpd
+import asyncore
+import sqlite3
+import time
+import pyclamav
+from geoip import geolite2
+'''
+
+## How does it work
+The script opens a connection and SMTP Server on port 1025 (for tests, should be changed to 25)
+an accepts every mail which is send to the server. It than logs the data and some other
+informations into an sqlite database.
+It was created to help analyze SPAM and malware which is sended per mail.
--- smsids.py
+++ smsids.py
@@ -19,26 +19,42 @@ import asyncore
 import sqlite3
 import time
 import pyclamav
+from geoip import geolite2
 
 class IDS_SMTPServer(smtpd.SMTPServer):
     
     def process_message(self, peer, mailfrom, rcpttos, data):
 	Now=time.time()
 	filename= "./tmp/"+ str(Now) + ".txt"
+
   	print 'ID		      ', Now
 	print 'Timestamp	      ', time.strftime('%X %x %Z')
-        print 'Receiving message from:', peer
-        print 'Message addressed from:', mailfrom
+        print 'Receiving message from:', peer[0]
+        match = geolite2.lookup(peer[0])
+	if match is not None:
+		Country = match.country
+	else:
+		Country = 'Unknown'
+
+	print 'Country		: ', Country
+
+	print 'Message addressed from:', mailfrom
         print 'Message addressed to  :', rcpttos
         print 'Message length        :', len(data)
-	print 'Message		     :', data
 	print 'Message store location:', filename
 	tempfile = open(filename, 'w')
 	tempfile.write(data)
 	tempfile.close()
 	ret=pyclamav.scanfile(filename)
 	print 'clamav		     :', ret
-        return
+
+	conn = sqlite3.connect('smsids.db', isolation_level=None)
+	c = conn.cursor()
+	logrow= (Now,time.strftime('%X %x %Z'), peer[0], peer[1], Country, mailfrom,rcpttos[0],len(data),data,filename,ret[1])
+	c.execute('''INSERT INTO log VALUES (?,?,?,?,?,?,?,?,?,?,?);''', logrow) 
+	conn.commit
+	conn.close
+	return
 
 print 'Server ready for connections...'
 server = IDS_SMTPServer(('0.0.0.0', 1025), None)
