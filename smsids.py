5520aa18adb8267fc5e677a46f6250336ca88a64
Joerg Stephan <jstephan@playbk>
2015-03-19 09:53:14 +0100

    V1.0.0 - adding comments :-)

--- smsids.py
+++ smsids.py
@@ -24,13 +24,18 @@ from geoip import geolite2
 class IDS_SMTPServer(smtpd.SMTPServer):
     
     def process_message(self, peer, mailfrom, rcpttos, data):
+	# Creating timestamp now, this represents ID and filename
 	Now=time.time()
 	filename= "./tmp/"+ str(Now) + ".txt"
 
+	# standard console output
+	# includes some basic features which are also written to sqlite
   	print 'ID		      ', Now
 	print 'Timestamp	      ', time.strftime('%X %x %Z')
         print 'Receiving message from:', peer[0]
         match = geolite2.lookup(peer[0])
+	# GeoIP return must be check against None. Cause local IP addresses would cause an NoneObject Type and crash
+	# the script.
 	if match is not None:
 		Country = match.country
 	else:
@@ -42,12 +47,26 @@ class IDS_SMTPServer(smtpd.SMTPServer):
         print 'Message addressed to  :', rcpttos
         print 'Message length        :', len(data)
 	print 'Message store location:', filename
+	# data to be written to tmp folder and pyclamav scaning afterwards.
 	tempfile = open(filename, 'w')
 	tempfile.write(data)
 	tempfile.close()
 	ret=pyclamav.scanfile(filename)
 	print 'clamav		     :', ret
-
+	
+	# Open sqlite db file and send data to it
+	# Values are like
+	# ID - represented via Unix-Timestamp
+	# time - via current time
+	# IP - the remote IP of the sender, peer[0]
+	# port - port used by sender , peer[1]
+	# Country Code - via GeoIP match.country
+	# mailfrom - the sender email addy
+	# rcpttos - to whom the mail was send
+	# len(data) - the length (chars) of the origin data msg
+	# data	- the data itself
+	# filename - file the data was saved to (needed for clamav scan), format always /tmp/ID.txt
+	# ClamAv return - what ClamAV has found
 	conn = sqlite3.connect('smsids.db', isolation_level=None)
 	c = conn.cursor()
 	logrow= (Now,time.strftime('%X %x %Z'), peer[0], peer[1], Country, mailfrom,rcpttos[0],len(data),data,filename,ret[1])
