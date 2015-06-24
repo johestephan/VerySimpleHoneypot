42f4bc6a50268f2979a1e48b6aacd1cd80292db8
root <root@beeswarm.dc.jsany.me>
2015-06-24 12:54:33 +0200

    changed facility

--- modules/syslogit.py
+++ modules/syslogit.py
@@ -6,10 +6,11 @@ def logit(AGENT, data):
 		data = map(str.strip, data)
 		sm = logging.getLogger(AGENT)
 		sm.setLevel(logging.INFO)
-		h = logging.handlers.SysLogHandler(address = '/dev/log')
+		h = logging.handlers.SysLogHandler(address = ('localhost',514),facility=logging.handlers.SysLogHandler.LOG_LOCAL6)
 		sm.addHandler(h)
 
 		sm.info(AGENT + " " + " ".join(data))
 		return 1	
 	except:
+		print err
 		return 0
