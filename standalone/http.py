66fd2fd3f3f3f1c8565cf9a02720e4ed083e0d7e
Joerg Stephan <jost2208@gmail.com>
2015-07-16 05:51:44 +0000

    up

--- standalone/http.py
+++ standalone/http.py
@@ -8,18 +8,24 @@ import sys
 sys.path.append('../modules/')
 import syslogit
 import IXFcheckMod
+import mypyfwa
+
+
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serveaddy = ('0.0.0.0', 2345)
+serveaddy = ('0.0.0.0', 80)
 sock.bind(serveaddy)
 sock.listen(1)
 
 while True:
-    msg = ""
-    con,addy = sock.accept()
-    msg += addy[0] + " -- " + IXFcheckMod.get_ip_intel_artillery_strip(addy[0])
-    data = con.recv(8192) # receive maximum 8K data
-    print addy
-    print data
-    con.sendall('HTTP/1.1 301 Moved Permanently')
-    
+    try:
+        msg = ""
+        con,addy = sock.accept()
+        #xf = addy[0] + " -- " + IXFcheckMod.get_ip_intel_artillery_strip(addy[0])
+        data = con.recv(8192) # receive maximum 8K data
+        dataarray = data.split('\n')
+        syslogit.logit("http",','.join(dataarray))
+        print mypyfwa.GETcheck(dataarray[0],addy[0])
+        con.sendall('HTTP/1.1 301 Moved Permanently')
+    except:
+        continue 
