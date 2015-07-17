01418d6c7ea8a2f023c4731114ca3b01dc7c6a53
Joerg Stephan <jost2208@gmail.com>
2015-07-17 11:36:12 +0200

    V1 operational

--- standalone/http.py
+++ standalone/http.py
@@ -9,6 +9,8 @@ sys.path.append('../modules/')
 import syslogit
 import IXFcheckMod
 import mypyfwa
+import datetime
+import time
 
 
 
@@ -25,12 +27,20 @@ while True:
         data = con.recv(8192) # receive maximum 8K data
         dataarray = data.split('\n')
 	rawf = open('/var/log/smsids_raw.log','a')
-        rawf.writeline(xf)
-	rawf.write(data)
+	ts = time.time()
+	st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+	rawf.write(st +'\n')
+	rawf.write('Source IP: '+ addy[0] +'\n')
+        rawf.write(xf +'\n')
+	rawf.write(data + '\n END OF DATA\n')
+	rawf.write('\n')
 	rawf.close()
         ters = mypyfwa.GETcheck(dataarray[0],addy[0])
         syslogit.logit("http",' -- '.join(ters))
-        con.send(r'''HTTP/1.0 200 OK 
-		   Content-Type: text/plain''')
-    except:
-        continue 
+        con.send("HTTP/1.1 200 OK\n"
+         +"Content-Type: text/html\n"
+         +"\n" # Important!
+         +"<html><body>Hello World</body></html>\n");
+	con.close()
+    except Exception, e:
+	print e
