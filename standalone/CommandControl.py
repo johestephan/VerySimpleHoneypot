85fe7b420a2156d8819ddc8e2fd45ea302c25c9e
Joerg Stephan <jost2208@gmail.com>
2015-07-19 10:02:35 +0000

    v1.1

--- standalone/CommandControl.py
+++ standalone/CommandControl.py
@@ -1,13 +1,37 @@
 import subprocess
 import os, signal
 import time
-import pygetch
+import sys
+import psutil
+
+
+def killchilds(cpid):
+	p = psutil.Process(cpid)
+        child_pid = p.children(recursive=True)
+	os.kill(child_pid[0].pid,9)
+
 
 # starting http
 http = subprocess.Popen("python http.py", shell = True)
 print 'started http with pid = ', http.pid
+https = subprocess.Popen("python https.py", shell = True)
+print 'started https with pid = ', https.pid
+smtp = subprocess.Popen("python smtp.py -p 25", shell = True)
+print 'started smtp with pid = ', smtp.pid
+
+
+
 print "Kill all processes? (Y/N) "
-#keystroke = pygetch()
-#if keystroke is "Y" or keystroke is "y":
-#	print "shutting down http"
-#	http.kill()
+keystroke = raw_input()
+if keystroke is "Y" or keystroke is "y":
+	print "shutting down http"
+        killchilds(http.pid)
+	http.terminate()
+	print "shutting down https"
+        killchilds(https.pid)
+	https.terminate()
+        print "shutting down smtp"
+        killchilds(smtp.pid)
+        smtp.terminate()
+
+sys.exit
