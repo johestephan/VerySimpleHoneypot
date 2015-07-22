3d2494a7be4b48cde006af324c9e2de4ef0e7bce
Joerg Stephan <jost2208@gmail.com>
2015-07-22 16:06:55 +0000

    v1.0 ALPHA

---
+++ standalone/mongodb.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+#
+# simple HTTP Server
+# as Part of SendMeSpamIDS
+
+import socket
+import sys
+sys.path.append('../modules/')
+import syslogit
+import IXFcheckMod
+import mypyfwa
+import datetime
+import time
+
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serveaddy = ('0.0.0.0', 27017)
+sock.bind(serveaddy)
+sock.listen(1)
+
+while True:
+    try:
+        msg = ""
+        con,addy = sock.accept()
+        xf = IXFcheckMod.get_ip_intel_artillery_strip(addy[0])
+        data = con.recv(8192) # receive maximum 8K data
+        dataarray = data.split('\n')
+	rawf = open('/var/log/smsids_raw.log','a')
+	ts = time.time()
+	st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+	rawf.write(st +'\n')
+	rawf.write('Source IP: '+ addy[0] +'\n')
+        rawf.write(xf +'\n')
+	rawf.write(data + '\n END OF DATA\n')
+	rawf.write('\n')
+	rawf.close()
+        # ters = mypyfwa.GETcheck(dataarray[0],addy[0])
+	ters = (addy[0].strip(), str(xf), str(len(data))) 
+        syslogit.logit("mongodb",' - '.join(ters))
+        con.send("Thanks for flying with us")
+	con.close()
+    except Exception, e:
+	print e
---
+++ standalone/rdp.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+#
+# simple RDP Server
+# as Part of SendMeSpamIDS
+
+import socket
+import sys
+sys.path.append('../modules/')
+import syslogit
+import IXFcheckMod
+import mypyfwa
+import datetime
+import time
+
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serveaddy = ('0.0.0.0', 3389)
+sock.bind(serveaddy)
+sock.listen(1)
+
+while True:
+    try:
+        msg = ""
+        con,addy = sock.accept()
+        xf = "test" #IXFcheckMod.get_ip_intel_artillery_strip(addy[0])
+        data = con.recv(8192) # receive maximum 8K data
+        dataarray = data.split('\n')
+	rawf = open('/var/log/smsids_raw.log','a')
+	ts = time.time()
+	st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+	rawf.write(st +'\n')
+	rawf.write('Source IP: '+ addy[0] +'\n')
+        rawf.write(xf +'\n')
+	rawf.write(data + '\n END OF DATA\n')
+	rawf.write('\n')
+	rawf.close()
+        # ters = mypyfwa.GETcheck(dataarray[0],addy[0])
+	ters = (addy[0].strip(), str(xf), str(len(data))) 
+        syslogit.logit("rdp",' - '.join(ters))
+        con.send("Thanks for flying with us")
+	con.close()
+    except Exception, e:
+	print e
