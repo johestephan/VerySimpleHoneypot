aa1450b860b1469f92e3c8de348f87b50c5585bf
Joerg Stephan <johest@localhost.localdomain>
2016-11-26 13:15:12 +0100

    new logging

---
+++ modules/logit.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.append('../modules/')
+import datetime
+import time
+
+def write_out(dataset, file="/var/log/smsids_syslog.log"):
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    with open(file, "ab") as mylogf:
+        mylogf.writeline("%s -- %s" %(st, dataset))
+    mylogf.close()
+    
+def log(agent, ip, data):
+    dataset = {agent : [ip, data]}
+    write_out(str(dataset))
