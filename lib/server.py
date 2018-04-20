ddaafb778f69babbe0943fc977bdfd552e369066
Joerg Stephan <johest@linux-4rf6.suse>
2018-04-20 08:53:20 +0200

    switch to syslog

--- lib/server.py
+++ lib/server.py
@@ -3,6 +3,7 @@ import sys
 import threading
 sys.path.append('./')
 import logit
+import syslog
 
 
 
@@ -18,7 +19,8 @@ def run(service, port, response=None):
             con,addy = sock.accept()
             data = con.recv(16000) # receive maximum 8K data
             dataarray = data.split('\n')
-            logit.log(service, addy[0], data)
+            #logit.log(service, addy[0], data)
+            syslog.syslog('''%s-Honey| %s | %s''' % (service, addy[0[, str(data)))
             con.send(response)
             con.close()
         except Exception, e:
